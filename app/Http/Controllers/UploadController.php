6d3dae9a4e27f0082f6c7333490497e66ca003e1
eiixy <990656271@qq.com>
2019-12-13 08:48:19 +0800

    初始化项目

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Sczts\Upload\Upload;
+use Sczts\Common\StatusCode;
+
+class UploadController extends Controller
+{
+    //
+    public function upload(Request $request,Upload $upload)
+    {
+        $file = $request->file('file');
+        $data = $upload->upload($file);
+        return $this->json(StatusCode::SUCCESS, $data);
+    }
+}
